05258caa68e6e9b143644d0fd74f5d2c8dfa31ac
Jonathan Calderaro <jcc2019.lambda@gmail.com>
2019-06-19 14:44:00 -0700

    Added Structure

---
+++ data/routes/postroutes.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+const db = require('./db');
+
+router.get('/posts', (req, res) => {
+
+
+})
+
+
+router.delete()
+
+router.post()
+
+module.exports = router
